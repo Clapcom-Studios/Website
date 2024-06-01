e18991479f06f6d0b5732cb2c931bca665bd248b
Urii98 <99950184+Urii98@users.noreply.github.com>
2024-06-01 21:20:17 +0200

    weapons page

---
+++ js/changeModel.js
@@ -0,0 +1,11 @@
+document.getElementById('shotgun').addEventListener('click', function() {
+    document.getElementById('model').src = 'weapons/shotgun.glb';
+});
+
+document.getElementById('submachine').addEventListener('click', function() {
+    document.getElementById('model').src = 'weapons/submachine.glb';
+});
+
+document.getElementById('plasmaCannon').addEventListener('click', function() {
+    document.getElementById('model').src = 'weapons/PlasmaCannon.glb';
+});
